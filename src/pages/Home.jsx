289d3e480eb64f1e6a584ea2c02b7dfad7778af8
Rizqi Fajri <rizqifajri51@gmail.com>
2024-08-10 14:43:47 +0700

    fix: bug import assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import photo1 from "../assets/background1.png";
 import photo2 from "../assets/background2.png";
 import photo3 from "../assets/background3.png";
-import title from "../assets/title.png";
+import title from "../assets/Title.png";
 import Button from "../components/Button";
 import Carousels from "../components/Carousels";
 
@@ -20,8 +20,8 @@ const Home = () => {
     >
       <div className='absolute inset-0 bg-gradient-to-t from-[#00D1FF] via-transparent to-transparent' />
       <div className='relative flex flex-col lg:flex-row justify-between items-end h-full gap-5 p-10'>
-        <div className="flex flex-col gap-5">
-          <img className="mt-56" src={title} alt='Title' />
+        <div className='flex flex-col gap-5'>
+          <img className='mt-56' src={title} alt='Title' />
           <Button title='Tentang program' />
         </div>
         <Carousels onImageClick={handleImageClick} />
